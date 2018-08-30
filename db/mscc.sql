a162d03833b0814e0688bfb1a39d4cb884420efe
Khusamov Sukhrob <khusamov@yandex.ru>
2018-08-31 00:13:33 +0300

    Обновлена схема базы данных

--- db/mscc.sql
+++ db/mscc.sql
@@ -1,31 +1,56 @@
 --
--- Файл сгенерирован с помощью SQLiteStudio v3.2.1 в Вс авг 19 13:18:42 2018
+-- Файл сгенерирован с помощью SQLiteStudio v3.2.1 в Вт авг 28 09:37:48 2018
 --
 -- Использованная кодировка текста: UTF-8
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
--- Таблица: Estimate
-CREATE TABLE Estimate (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Number INTEGER NOT NULL, CreationDate DATE, Client TEXT, Note TEXT);
+-- Таблица: estimate
+DROP TABLE IF EXISTS estimate;
+CREATE TABLE estimate (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, jobCategoryId INTEGER REFERENCES jobCategory (Id) NOT NULL, number INTEGER NOT NULL, creationDate DATE, client TEXT, note TEXT);
 
--- Таблица: EstimateItem
-CREATE TABLE EstimateItem (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, EstimateId INTEGER REFERENCES Estimate (ID) NOT NULL, Number INTEGER NOT NULL, ProductId INTEGER REFERENCES Product (ID), Price REAL, Extra REAL, PriceWithExtra REAL, Quantity INTEGER, Cost REAL, Discount REAL, Total REAL, HourNorm REAL, " ManagerNote" TEXT, Note TEXT, Footnote TEXT, ProductGroupId INTEGER REFERENCES ProductGroup (ID));
+-- Таблица: estimateItem
+DROP TABLE IF EXISTS estimateItem;
+CREATE TABLE estimateItem (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, estimateId INTEGER REFERENCES Estimate (Id) NOT NULL, productId INTEGER REFERENCES product (Id), productGroupId INTEGER REFERENCES productGroup (Id), number INTEGER NOT NULL, price REAL, extra REAL, priceWithExtra REAL, quantity INTEGER, cost REAL, discount REAL, total REAL, hourNorm REAL, managerNote TEXT, note TEXT, footNote TEXT);
 
--- Таблица: ExtraExpenses
-CREATE TABLE ExtraExpenses (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ValueRate REAL, Value REAL, Note TEXT, Type TEXT, Number INTEGER NOT NULL);
+-- Таблица: extraExpenses
+DROP TABLE IF EXISTS extraExpenses;
+CREATE TABLE extraExpenses (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, estimateId INTEGER REFERENCES estimate (id) NOT NULL, number INTEGER NOT NULL, valueRate REAL, value REAL, note TEXT, type TEXT);
 
--- Таблица: JobCategory
-CREATE TABLE JobCategory (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Name TEXT NOT NULL, HourNormCost REAL);
+-- Таблица: jobCategory
+DROP TABLE IF EXISTS jobCategory;
+CREATE TABLE jobCategory (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, hourNormCost REAL);
 
--- Таблица: LaborCost
-CREATE TABLE LaborCost (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ProductId INTEGER REFERENCES Product (ID) NOT NULL, JobCategoryId INTEGER REFERENCES JobCategory (ID), HourNorm REAL NOT NULL);
+-- Таблица: laborCost
+DROP TABLE IF EXISTS laborCost;
+CREATE TABLE laborCost (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, productId INTEGER REFERENCES product (Id) NOT NULL, jobCategoryId INTEGER REFERENCES jobCategory (Id), hourNorm REAL NOT NULL);
 
--- Таблица: Product
-CREATE TABLE Product (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, VendorCode TEXT NOT NULL, Name TEXT NOT NULL, Unit TEXT, ManagerNote TEXT, Note TEXT, Footnote TEXT, ProductGroupId INTEGER REFERENCES ProductGroup (ID));
+-- Таблица: product
+DROP TABLE IF EXISTS product;
+CREATE TABLE product (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, productGroupId INTEGER REFERENCES productGroup (Id), vendorCode TEXT NOT NULL, name TEXT NOT NULL, unit TEXT, managerNote TEXT, note TEXT, footNote TEXT);
 
--- Таблица: ProductGroup
-CREATE TABLE ProductGroup (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL);
+-- Таблица: productGroup
+DROP TABLE IF EXISTS productGroup;
+CREATE TABLE productGroup (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);
+
+-- Триггер: dlaborCostJC
+DROP TRIGGER IF EXISTS dlaborCostJC;
+CREATE TRIGGER dlaborCostJC AFTER DELETE ON jobCategory BEGIN delete from laborCost where laborCost.jobCategoryId = old.id; END;
+
+-- Триггер: dLaborCostP
+DROP TRIGGER IF EXISTS dLaborCostP;
+CREATE TRIGGER dLaborCostP AFTER DELETE ON product FOR EACH ROW BEGIN delete from laborCost where laborCost.productId = old.id; END;
+
+-- Триггер: ilaborCostJC
+DROP TRIGGER IF EXISTS ilaborCostJC;
+CREATE TRIGGER ilaborCostJC AFTER INSERT ON jobCategory BEGIN INSERT INTO laborCost (productId, jobCategoryId, hourNorm) 
+SELECT product.id, new.id, 0 FROM product; END;
+
+-- Триггер: iLaborCostP
+DROP TRIGGER IF EXISTS iLaborCostP;
+CREATE TRIGGER iLaborCostP AFTER INSERT ON product FOR EACH ROW BEGIN INSERT INTO laborCost (productId, jobCategoryId, hourNorm) 
+select new.id, jc.id, 0 from jobCategory jc; END;
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
