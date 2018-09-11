3ff8af368135c1e7a347ef1fef5b21eea77e97de
Khusamov Sukhrob <khusamov@yandex.ru>
2018-09-11 13:05:04 +0300

    Компонент Button: Добавлено свойство disabled

--- src/library/button/Button.tsx
+++ src/library/button/Button.tsx
@@ -4,12 +4,18 @@ import './Button.scss';
 interface IButtonProps {
 	onClick?: () => void;
 	type?: string;
+	disabled?: boolean;
 }
 
 export default class Button extends Component<IButtonProps> {
 	public render(): ReactNode {
 		return (
-			<button className='Button' onClick={this.props.onClick} type={this.props.type}>
+			<button
+				className='Button'
+				onClick={this.props.onClick}
+				type={this.props.type}
+				disabled={this.props.disabled}
+			>
 				{this.props.children}
 			</button>
 		);
