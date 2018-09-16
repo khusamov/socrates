2599e2828c71b73e82ed96dc20ca4d108e0a7f73
Khusamov Sukhrob <khusamov@yandex.ru>
2018-09-16 20:12:10 +0300

    Добавлен комментарий ProductGroup

--- src/components/settings/productGroup/ProductGroup.ts
+++ src/components/settings/productGroup/ProductGroup.ts
@@ -1,5 +1,8 @@
 import {Resource, IResource, Store} from '@library/rest/index';
 
+/**
+ * Группы товаров/услуг.
+ */
 export default class ProductGroup extends Resource<IProductGroup> {
 	public static store = new Store<typeof ProductGroup, ProductGroup>(ProductGroup);
 }
